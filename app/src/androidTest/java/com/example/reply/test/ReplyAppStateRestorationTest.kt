7f0d1651abf2ef0694245ea37bc312956d0495c7
j4n9k4 <janukananayakkara@gmail.com>
2025-03-18 18:03:50 +0530

    Tested of configuration changes in expanded mode

--- app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -2,9 +2,13 @@ package com.example.reply.test
 
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.assertAny
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.StateRestorationTester
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.example.reply.data.local.LocalEmailsDataProvider
@@ -50,5 +54,33 @@ class ReplyAppStateRestorationTest {
         ).assertExists()
     }
 
+    @Test
+    fun expandedDevice_selectedEmailEmailRetained_afterConfigChange()
+    {
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent {
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
+        }
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        composeTestRule.onNodeWithTagForStringId(com.example.reply.R.string.details_screen).onChildren()
+            .assertAny(hasAnyDescendant(hasText(
+                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+            )))
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        composeTestRule.onNodeWithTagForStringId(com.example.reply.R.string.details_screen).onChildren()
+            .assertAny(hasAnyDescendant(hasText(
+                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+            )))
+
+
+    }
 
 }
--- app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
+++ app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
@@ -49,6 +49,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -75,6 +76,7 @@ fun ReplyDetailsScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = MaterialTheme.colorScheme.inverseOnSurface)
+                .testTag(stringResource(R.string.details_screen))
         ) {
             item {
                 if(isFullScreen) {
