2eadfd4e263290596df85c2d5f67cbcc88bab586
j4n9k4 <janukananayakkara@gmail.com>
2025-03-18 17:32:06 +0530

    Tested of configuration changes in compact mode

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -0,0 +1,54 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.reply.data.local.LocalEmailsDataProvider
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+
+class ReplyAppStateRestorationTest {
+    @get: Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_selectEmailEmailRetained_afterConfigChange()
+    {
+            val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent {
+            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
+        }
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)
+        ).performClick()
+
+        composeTestRule.onNodeWithContentDescriptionForStringId(
+            com.example.reply.R.string.navigation_back
+        ).assertExists()
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertExists()
+
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        composeTestRule.onNodeWithContentDescriptionForStringId(
+            com.example.reply.R.string.navigation_back
+        ).assertExists()
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertExists()
+    }
+
+
+}
